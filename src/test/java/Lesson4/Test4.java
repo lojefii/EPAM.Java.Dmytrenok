f977e75fc53004580d0d7359f433663047010d15
Ruxen <dmitrenokb@gmail.com>
2021-11-24 00:35:58 +0200

    Lesson 5

--- src/test/java/Lesson4/Test4.java
+++ src/test/java/Lesson4/Test4.java
@@ -9,7 +9,7 @@ public class Test4 {
 
     @Test
     void SumGeometricElements() {
-        assertEquals(262.5, Task4.SumGeometricElements(150.0, 0.5,30.0));
-        assertEquals(1076.92, Task4.SumGeometricElements(760.0, 0.3,12.0));
+        assertEquals(262.5, Task4.SumGeometricElements(150.0, 0.5, 30.0));
+        assertEquals(1076.92, Task4.SumGeometricElements(760.0, 0.3, 12.0));
     }
 }
